8937a57c3afea665762a3db065560bab96a350bc
Vlad Burlacu <vladburlacu1337@gmail.com>
2022-10-11 12:01:11 +0200

    updated the view and post model and changed user to author

--- Hello-World/resources/views/post.blade.php
+++ Hello-World/resources/views/post.blade.php
@@ -14,7 +14,7 @@
     <h1>{!! $post->title !!}</h1>
 
     <p>
-        By <a href="#">{{$post->user->name}}</a> <a href="/categories/{{ $post->category->slug }}"> {{ $post->category->name }} </a>
+        By <a href="#">{{$post->author->name}}</a> <a href="/categories/{{ $post->category->slug }}"> {{ $post->category->name }} </a>
     </p>
 
     <div>
