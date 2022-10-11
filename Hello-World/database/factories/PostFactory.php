aa265ef5c34c52c0ed5568f42f98296d76964765
Vlad Burlacu <vladburlacu1337@gmail.com>
2022-10-11 10:59:57 +0200

    edited the Category factory and added a slug in the Post factory

--- Hello-World/database/factories/PostFactory.php
+++ Hello-World/database/factories/PostFactory.php
@@ -19,6 +19,7 @@ class PostFactory extends Factory
             'user_id' => User::factory(),
             'category_id' => Category::factory(),
             'title' => $this->faker->sentence,
+            'slug' => $this->faker->slug,
             'excerpt' => $this->faker->sentence,
             'body' => $this->faker->paragraph
         ];
