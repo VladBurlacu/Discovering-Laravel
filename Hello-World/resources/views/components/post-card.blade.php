b34aaca06199fadd1ae561cef1e4ac221457cf4c
Vlad Burlacu <vladburlacu1337@gmail.com>
2022-10-17 21:48:03 +0200

    added new component for posts grid

--- Hello-World/resources/views/components/post-card.blade.php
+++ Hello-World/resources/views/components/post-card.blade.php
@@ -1,5 +1,7 @@
+@props(['post'])
+
 <article
-    class="transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl">
+    {{ $attributes->merge(['class' => 'transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl']) }}>
     <div class="py-6 px-5">
         <div>
             <img src="./images/illustration-3.png" alt="Blog Post illustration" class="rounded-xl">
@@ -35,10 +37,10 @@
 
             <footer class="flex justify-between items-center mt-8">
                 <div class="flex items-center text-sm">
-                    <img src="./images/lary-avatar.svg" alt="Lary avatar">
+                    <img src="./images/dino.gif" alt="Lary avatar" height="80" width="80">
                     <div class="ml-3">
                         <h5 class="font-bold">{{ $post->author->name }}</h5>
-                        <h6>Mascot at Laracasts</h6>
+                        <h6>Mascot</h6>
                     </div>
                 </div>
 
