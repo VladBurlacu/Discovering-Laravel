343ac761fdc1e2ae3fae829e9452654ee8a8211e
Vlad Burlacu <vladburlacu1337@gmail.com>
2022-10-17 16:02:11 +0200

    added username in users table

--- Hello-World/resources/views/post.blade.php
+++ Hello-World/resources/views/post.blade.php
@@ -14,7 +14,7 @@
     <h1>{!! $post->title !!}</h1>
 
     <p>
-        By <a href="#">{{$post->author->name}}</a> <a href="/categories/{{ $post->category->slug }}"> {{ $post->category->name }} </a>
+        By <a href="/authors/{{$post->author->name}}">{{$post->author->name}}</a> in <a href="/categories/{{ $post->category->slug }}"> {{ $post->category->name }} </a>
     </p>
 
     <div>
--- Hello-World/routes/web.php
+++ Hello-World/routes/web.php
@@ -1,13 +1,14 @@
 <?php
 
 use App\Models\Category;
+use App\Models\User;
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
 
     return view('posts', [
-        'posts' => Post::latest()->with('category')->get()
+        'posts' => Post::latest()->with('category', 'author')->get()
     ]);
 });
 
@@ -20,5 +21,11 @@ Route::get('posts/{post:slug}', function (Post $post) {
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
         'posts' => $category->posts
+    ]);
+});
+
+Route::get('authors/{author}', function (User $author) {
+    return view('posts', [
+        'posts' => $author->posts
     ]);
 });
